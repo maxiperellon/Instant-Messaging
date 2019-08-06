3a35cd7ba6bc5d935da3c781f0e2c2ba5fef390d
Ornella Grasso <you@example.com>
2019-08-06 18:41:26 -0300

    servidor quedo escuchando

--- server/server.c
+++ server/server.c
@@ -2,16 +2,16 @@
 
 int main(int argc, char *argv[]) 
 { 
-    int sockfd=0, conx_acept=0, cont=0, l, t_struct; 
+    int sockfd=0, conx_acept=0, cont=0, l, i, b, t_struct; 
     struct sockaddr_in servaddr;
-	struct sockadrr_in servcli;
+	//struct sockadrr_in servcli;
 	pthread_t idHilo;
 
   
     // socket create and verification 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); 
 	
-    if (sockaddr < 0){ 
+    if (sockfd < 0){ 
         printf("socket creation failed...\n"); 
         exit(0); 
     } 
@@ -19,18 +19,18 @@ int main(int argc, char *argv[])
     // assign IP, PORT 
     servaddr.sin_family = AF_INET; 
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(PORT); 
+    servaddr.sin_port = htons(SERVER_PORT); 
 
 	t_struct =  sizeof(struct sockaddr); /* Obtenemos el tamaño de la estructura en bites */
 
-	b = bind(sockfd, (struct sockaddr *)&servaddr, t_struct; /* Asociamos el socket a un puerto local */
+	b = bind(sockfd, (struct sockaddr *)&servaddr, t_struct); /* Asociamos el socket a un puerto local */
 
-	if (b) < 0) { /* Comprobamos si se asocio el socket a un puerto local */
+	if (b < 0) { /* Comprobamos si se asocio el socket a un puerto local */
     	printf("ERROR: error de bind");
     	return 1;
   	}
   
-	l = listen(s, NUM_CLIENTES) /* Ponemos el puerto a la escucha */
+	l = listen(sockfd, NUM_CLIENTES); /* Ponemos el puerto a la escucha */
 
 	if (l < 0) { /* Comprobamos si se puso a la escucha el puerto local */
 		printf("ERROR: error de listen");
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
 
 	while(1)
 	{
-		conx_acept=accept(s, (struct sockaddr*)&servaddr, &t_struct);
+		conx_acept=accept(sockfd, (struct sockaddr*)&servaddr, &t_struct);
 		if(conx_acept>0)
 		{ 			
 
