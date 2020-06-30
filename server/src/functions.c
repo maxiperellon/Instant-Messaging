8f1cb640a2cd1003ade8a3b91baec22b6b41fa5a
maxiperellon <maxyperellon@gmail.com>
2020-06-30 12:19:19 -0300

    modificaciones_01

--- server/src/functions.c
+++ server/src/functions.c
@@ -75,7 +75,7 @@ int search_client(char* user) {
 
 void* welcome(int id, char *buffer) {
     strcpy(buffer, "\tHola ");
-    strcat(buffer, s_cli[id].user);
+    strcat(buffer, s_cli[id].username);
     strcat(buffer, "!!!");
 
     if (s_cli[id].sign_in == 1) {
@@ -88,7 +88,7 @@ void* add_client(int id, char *buffer, char *name) {
     int len = strlen(buffer);
     //Le quitamos el ADD
     cut_buff(name, buffer, 4, len-4);
-    strcpy(s_cli[id].user, name);
+    strcpy(s_cli[id].username, name);
     //Se informa a todos menos a él mismo y al que se haya ido
     strcpy(buffer, "El usuario ");
     strcat(buffer, name);
@@ -106,7 +106,7 @@ void* client_list(int id) {
     //Se envia al cliente todos los usuarios menos los que hayan abandonado la sesión y el de el propio
     for(int i = 0; i < clients; i++) {
         if(i != id && s_cli[i].sign_in == 1 && s_cli[i].status == 0) {
-            send(s_cli[id].socket, s_cli[i].user, MAXLINE, 0);
+            send(s_cli[id].socket, s_cli[i].username, MAXLINE, 0);
         }
     }
     return 0;
@@ -115,9 +115,9 @@ void* client_list(int id) {
 void* end_chat(int id, char *buffer) {
     //Se informa a todos menos a él mismo y al que se haya ido
     strcpy(buffer, "El usuario ");
-    strcat(buffer, s_cli[id].user);
+    strcat(buffer, s_cli[id].username);
     strcat(buffer, " ha abandonado el chat.");
-    bzero(s_cli[id].user, MAXLINE);
+    bzero(s_cli[id].username, MAXLINE);
     for (int i = 0; i < clients; i++) {
         if (i != id && s_cli[i].sign_in == 1) {
             send(s_cli[i].socket, buffer, MAXLINE, 0);
@@ -135,19 +135,21 @@ void* chat_with_other_user(int id, char *buffer, char *name, char *temp) {
     //Se obtiene el socket destino
     int sock_destination = search_client(name);
     int len=strlen(name);
-    strcpy(name, s_cli[id].user);
+    strcpy(name, s_cli[id].username);
     strcat(name, ": ");
     //Recortamos el CHAT, el nombre, y los dos espacios hasta el mensaje(se suma solo uno (un espacio)
     cut_buff(temp, buffer, 5+len+1, MAXLINE-(5+len+1));
     strcat(name, temp); // se envia el mensaje completo con el nick del usuario
+
     if (sock_destination != 0){
         send(sock_destination, name, MAXLINE, 0);
         s_cli[id].status = 1;
-        //s_cli[sock_destination].status = 1;
+        s_cli[sock_destination].status = 1;
     } else {
         char *msg = "El usuario con en el que desea chatear no se encuentra en la sala.";
         send(s_cli[id].socket, msg, MAXLINE, 0);
     }
+
     return 0;
 }
 
